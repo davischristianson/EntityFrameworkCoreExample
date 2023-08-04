972c584dfda12e0b9d53a69529c31f5ffb0c8d19
davischristianson <dav.christian10@gmail.com>
2023-08-03 18:47:17 -0700

    Added EF Core SWL Server Package and created the database context class

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkCoreExample
 {
-    internal class StudentContext
+    // EF Core Getting Started
+    // https://learn.microsoft.com/en-us/ef/core/get-started/overview/install
+    internal class StudentContext : DbContext
     {
 
     }
